82fb5d419899806037ff5a8ae8dffcd24a500e22
Shirkenitin347 <116098192+Shirkenitin347@users.noreply.github.com>
2023-01-14 18:17:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,6 +30,7 @@ resource "azurerm_subnet" "my_terraform_subnet" {
   resource_group_name  = azurerm_resource_group.webServer_rg.name
   virtual_network_name = azurerm_virtual_network.webServer_network.name
   address_prefixes     = ["10.0.1.0/24"]
+	
 }
 
 # Create public IPs
